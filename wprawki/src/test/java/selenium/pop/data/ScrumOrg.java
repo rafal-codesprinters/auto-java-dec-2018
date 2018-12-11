17e419c4aba13fe7d7bd5eb4232c6435a81a9b16
Rafał Markowicz <rafal@codesprinters.com>
2018-12-11 13:32:40 +0100

    Added JUnit5 lesson

--- wprawki/src/test/java/selenium/pop/data/ScrumOrg.java
+++
@@ -1,7 +0,0 @@
-package selenium.pop.data;
-
-public abstract class ScrumOrg {
-	public static final String NAME = "Scrum.org";
-	public static final String URL = "https://www.scrum.org/";
-	public static final String DISPLAYED_URL = "https://www.scrum.org/";
-}
